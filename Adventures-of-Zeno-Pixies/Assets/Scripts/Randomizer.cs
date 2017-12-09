61344d5f53818efa6dcc4d5294caec0ae3e6a256
LD-PC\LD <bvv2ldskt@mail.ru>
2017-12-09 19:07:22 +0200

    Regrouping files for instantiation, updated spawning script, GUI in experimentl scene. Need to implement connections mechanics. New prefab and script for it.

--- Adventures-of-Zeno-Pixies/Assets/Scripts/Randomizer.cs
+++ Adventures-of-Zeno-Pixies/Assets/Scripts/Randomizer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Randomizer : MonoBehaviour {
 
@@ -23,20 +24,20 @@ public class Randomizer : MonoBehaviour {
         return vec;
     }
 
-    public List<Vector3> GenerateNodePosition (int num, int grouping)
+    public List<Vector3> GenerateNodePosition (int num, int grouping = 4)
     {
         
         List <Vector3> PositionList = new List<Vector3>();
-        for (int i=0; i > num; i++)
+        for (int i=0; i < num; i++)
         {
             PositionList.Add(randomer());
         }
-        for (int i = 0; i > num; i++)
+        for (int i = 0; i < num; i++)
         {
             bool distMax = true;
             bool distMin = false;
             int connections = 0;
-            for (int j = 0; j > num; j++)
+            for (int j = 0; j < num; j++)
             {
                 if (i != j)
                 {
@@ -49,7 +50,7 @@ public class Randomizer : MonoBehaviour {
                     }
                 }
             }
-            if (distMax||distMin||connections>grouping)
+            if (distMax||distMin||connections>grouping||connections<1)
             {
                 PositionList.RemoveAt(i);
                 PositionList.Add(randomer());
@@ -63,12 +64,12 @@ public class Randomizer : MonoBehaviour {
     public int[] StartEnd(List<Vector3> list)
     {
         int[] StartEnd = new int[2];
-        for (int i = 0; i > list.Count; i++)
+        for (int i = 0; i < list.Count; i++)
         {
             float MaxDist = Vector3.Distance(list[0], list[1]);
-            for (int o = 0; o > list.Count; o++)
+            for (int o = 0; o < list.Count; o++)
             {
-                for (int j = i + 1; j > list.Count; j++)
+                for (int j = i + 1; j < list.Count; j++)
                 {
                     if (Vector3.Distance(list[i], list[j]) > MaxDist)
                     {
@@ -83,6 +84,25 @@ public class Randomizer : MonoBehaviour {
 
     }
 
-
+        public void Spawner()
+    {
+        GameObject Difficulty = GameObject.Find("Difficulty");
+        GameObject Amount = GameObject.Find("NodeAmount");
+        GameObject Grouping = GameObject.Find("Grouping");
+        List<Vector3> NodePos = new List<Vector3>();
+        NodePos = GenerateNodePosition(Mathf.RoundToInt(Amount.gameObject.GetComponent<Slider>().value), Mathf.RoundToInt(Grouping.gameObject.GetComponent<Slider>().value));
+        for (int i = 0; i < NodePos.Count; i++)
+        {
+            GameObject NewNode = GameObject.Instantiate((GameObject)Resources.Load("NodePrefab"), NodePos[i], Quaternion.identity);
+            NewNode.gameObject.name = "Node " + i.ToString();
+        }
+        Vector3 pos = randomer();
+        GameObject NPixie = GameObject.Instantiate((GameObject)Resources.Load("PixiePrefab"), pos, Quaternion.identity);
+        int[] SE = StartEnd(NodePos);
+        GameObject StartNode = GameObject.Find("Node " + SE[0].ToString());
+        GameObject EndNode = GameObject.Find("Node " + SE[1].ToString());
+        NPixie.gameObject.GetComponent<Pixie>().collapsedNode_ = StartNode.gameObject.GetComponent<Node>();
+        EndNode.gameObject.GetComponent<NodeTrigger>().WinTrigger_ = true;
+    }
 
 }
---
+++ Adventures-of-Zeno-Pixies/Assets/Scripts/TextUpdater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextUpdater : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Slider Slider;
+    public Text Text;
+
+    public void TextUpdate()
+    {
+        Text.text = Slider.value.ToString();
+
+    }
+}
